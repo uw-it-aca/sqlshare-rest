b6ecd52061b2c130ccf514764f595f7b1466fabc
Stephen De Vight <devights@gmail.com>
2015-04-28 14:52:55 -0700

    committing first stab at mssql support

---
+++ sqlshare_rest/backend/mssql.py
@@ -0,0 +1,113 @@
+from sqlshare_rest.backend.base import DBInterface
+from sqlshare_rest.models import User
+from django.db import connection
+from django.conf import settings
+import re
+import hashlib
+
+# Basic permissions to everything
+# grant all on *.* to <user>
+# Ability to give new users permission to their database
+# grant grant option on *.* to <user>
+
+
+class MSSQLBackend(DBInterface):
+    def create_db_user(self, username, password):
+        pass
+        # cursor = connection.cursor()
+        # cursor.execute("CREATE USER %s IDENTIFIED BY %s", (username, password))
+        # return
+
+    def get_db_username(self, user):
+        pass
+        # MySQL only allows 16 character names.  Take the md5sum of the
+        # username, and hope it's unique enough.
+        # hash_val = hashlib.md5(user.encode("utf-8")).hexdigest()[:11]
+        # test_value = "meta_%s" % (hash_val)
+        #
+        # try:
+        #     existing = User.objects.get(db_username=test_value)
+        #     msg = "Hashed DB Username already exists! " \
+        #           "Existing: %s, New: %s" % (exists.username, user)
+        #     raise Exception(msg)
+        #
+        # except User.DoesNotExist:
+        #     # Perfect!
+        #     pass
+        #
+        # return test_value
+
+    def get_db_schema(self, user):
+        # stripped down schema name - prevent quoting issues
+        # return re.sub('[^a-zA-Z0-9]', '_', user)
+        pass
+
+    # Maybe this could become separate files at some point?
+    def create_db_schema(self, username, schema):
+        pass
+        # cursor = connection.cursor()
+        # # MySQL doesn't allow placeholders on the db name here.
+        # # This is protected by the get_db_schema method, which only allows
+        # # a-z, 0-9, and _.
+        # cursor.execute("CREATE DATABASE %s" % schema)
+        #
+        # # Using placeholders here results in bad syntax
+        # cursor.execute("GRANT ALL on %s.* to %s" % (schema, username))
+
+    def remove_db_user(self, user):
+        # cursor = connection.cursor()
+        # # MySQL doesn't let the username be a placeholder in DROP USER.
+        # cursor.execute("DROP USER %s" % (user))
+        return
+
+    def remove_schema(self, schema):
+
+        pass
+        # cursor = connection.cursor()
+        # # MySQL doesn't allow placeholders on the db name here.
+        # # This is protected by the get_db_schema method, which only allows
+        # # a-z, 0-9, and _.
+        # schema = self.get_db_schema(schema)
+        # cursor.execute("DROP DATABASE %s" % schema)
+
+    def _disconnect_connection(self, connection):
+        pass
+        # connection["connection"].close()
+
+    def create_view(self, name, sql, user):
+        # view_sql = "CREATE OR REPLACE VIEW %s AS %s" % (name, sql)
+        # self.run_query(view_sql, user)
+        return
+
+    def run_query(self, sql, user):
+        pass
+        # connection = self.get_connection_for_user(user)
+        # cursor = connection.cursor()
+        # cursor.execute(sql)
+        # return cursor.fetchall()
+
+    def _create_user_connection(self, user):
+        pass
+        # username = user.db_username
+        # password = user.db_password
+        # schema = user.schema
+        #
+        # host = settings.DATABASES['default']['HOST']
+        # port = settings.DATABASES['default']['PORT']
+        #
+        # kwargs = {
+        #     "user": username,
+        #     "passwd": password,
+        #     "db": schema,
+        # }
+        #
+        # if host:
+        #     kwargs["host"] = host
+        #
+        # if port:
+        #     kwargs["port"] = port
+        #
+        # import pymysql
+        # conn = pymysql.connect(**kwargs)
+        #
+        # return conn
---
+++ sqlshare_rest/test/backend/mssql.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+
+from sqlshare_rest.util.db import is_mssql, get_backend
+import unittest
+
+@unittest.skipUnless(is_sqlite3(), "Only test with sqlite3")
+class TestMSSQLBackend(TestCase):
+
+    def test_create_user(self):
+        pass
+        # backend = get_backend()
+        # user = backend.get_user("test_user_tcu1")
+
+    # def test_run_query(self):
+    #     backend = get_backend()
+    #     user = backend.get_user("test_user_trq1")
+    #     result = backend.run_query("select (5)", user)
+    #     self.assertEquals(len(result), 1)
+    #     self.assertEquals(result[0][0], 5)
+    #
+    #     result = backend.run_query('select (10) union select ("a")', user)
+    #     self.assertEquals(len(result), 2)
+    #     self.assertEquals(result[0][0], 10)
+    #     self.assertEquals(result[1][0], "a")
+    #
+    # def test_create_view(self):
+    #     backend = get_backend()
+    #     user = backend.get_user("test_user_view1")
+    #     backend.create_view("test_view", "SELECT (1) UNION SELECT ('a')", user)
+    #
+    #     try:
+    #         result = backend.run_query("SELECT * FROM test_view", user)
+    #         self.assertEquals([(1,),(u'a',)], result)
+    #     except Exception as ex:
+    #         print ("E: ", ex)
+    #
+    #     backend.close_user_connection(user)
