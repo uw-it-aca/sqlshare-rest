b0718d280bd95bd2da734ac06a94a215e769f23c
Patrick Michaud <pmichaud@uw.edu>
2016-11-18 17:03:59 -0800

    Adding placeholder google oauth vars

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+GOOGLE_OAUTH2_CLIENT_ID = "XXX"
+GOOGLE_OAUTH2_CLIENT_SECRET = "XXX"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
